fa09c70bc97fe9e4838da752c8cd86ee30f11f1d
LaraMol <99068065@mydavinci.nl>
2022-03-15 10:21:06 +0100

    text

--- clickerv3.py
+++ clickerv3.py
@@ -23,11 +23,54 @@ def on_leave(e):
 root.bind("<Enter>", on_enter)
 root.bind("<Leave>", on_leave)
 
+def Upbutton(event):
+    global Numbers
+    global Is_up
+    Is_up = True 
+    Numbers += 1
+    counter['text'] = Numbers
+    if Numbers > 0:
+        root["background"] = 'green'
+    elif Numbers < 0:
+        root["background"] = 'red'
+    else:
+        root["background"] = 'pink'
+
+
+def Downbutton(event):
+    global DOWN
+    global Numbers
+    global Is_up
+    Is_up = False
+    Numbers -= 1
+    counter['text'] = Numbers
+    if Numbers >0:
+        root["background"] = 'green'
+    elif Numbers <0:
+        root["background"] = 'red'
+    else:
+        root["background"] = 'pink'
+
+
+
+def Spacelabel(event):
+    global Clickerlabel
+    global Numbers
+    global DOWNN
+    global UP
+    global Is_up
+    if Is_up == True: 
+        Numbers = Numbers * 3
+    else: 
+        Numbers = Numbers / 3
+        Numbers = round((Numbers/3),2)
+    counter['text'] = Numbers
+
 
 def UP():
     global Numbers
     global Is_up
-    Is_up = True # Zelfde hier
+    Is_up = True 
     Numbers += 1
     counter['text'] = Numbers
     if Numbers > 0:
@@ -40,7 +83,7 @@ def UP():
 def DOWN():
     global Numbers
     global Is_up
-    Is_up = False #Dit is een vergelijking geen aanpassng juiste manier is Is_Up = False
+    Is_up = False
     Numbers -= 1
     counter['text'] = Numbers
     if Numbers >0:
@@ -92,5 +135,8 @@ counter.pack(
 )
 
 counter.bind('<Double-Button>',Clickerlabel)
+root.bind('+', Upbutton)
+root.bind('-', Downbutton)
+root.bind('<space>', Spacelabel )
 
 root.mainloop()
